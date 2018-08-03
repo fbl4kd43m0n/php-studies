d1d04c8b9be5f551a70455f1177587ff0a41c116
Franciny Salles <35750347+fbl4kd43m0n@users.noreply.github.com>
2018-08-03 14:08:35 -0300

    Create sha1.php
    Using sha1

---
+++ sha1.php
@@ -0,0 +1,26 @@
+<?php
+@$login = $_POST["login"];
+@$senha = $_POST["senha"];
+
+if($senha != ""){
+	$senha = sha1($senha);
+	
+	echo "Sua senha &eacute;: ".$senha;
+}
+?>
+
+<!doctype html public "-/w3v//dtd xhtml 1.0 Transitional//en" "http://www.w3.org/TR/xhtml1/">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>::Hash no PHP::</title>
+</head>
+
+<body>
+	<form action="" method="post">
+		<input type="text" name="login" />
+		<input type="password" name="senha" />
+		<input type="submit" />
+	</form:
+</body>
+</html>
